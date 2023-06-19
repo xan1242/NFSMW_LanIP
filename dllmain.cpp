a7f3c4c19168765561037114724239d6f91632d7
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-06-19 22:56:19 +0200

    implement patches for LAN mode over internet

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,7 +10,12 @@
 #include "WndProcWalker.hpp"
 #include <d3d9.h>
 #include <WinSock2.h>
+#include <wininet.h>
 #include "upnpnat/upnpnat.h"
+#include "Network.h"
+#include <thread>
+
+#pragma comment(lib, "wininet.lib")
 
 #define GAME_HWND_ADDR 0x00982BF4
 #define GAME_D3DDEVICE_ADDR 0x00982BDC
@@ -33,15 +38,130 @@ uintptr_t RenderLoopFuncAddr;
 uintptr_t RenderResetFuncAddr;
 
 bool bShowIPInputWindow = false;
-bool bShownIPWindowOnce = false;
 bool bIPWindowOnceFlag = false;
 
 uint32_t LastPortMapAddr = 0;
 bool bUseUPnP = false;
+bool bOnlineGame = false;
+
+uint32_t external_addr = 0;
+char external_addr_str[128];
+constexpr const char* ipcheckdomain = "http://myexternalip.com/raw";
+bool bCurrentlyCatchingIP = false;
+bool bLocalChallengeActive = false;
 
 // needed because MW reports a tiny viewport for some reason
 void(__stdcall* GetCurrentRes)(unsigned int* OutWidth, unsigned int* OutHeight) = (void(__stdcall*)(unsigned int*, unsigned int*))0x6C27D0;
 #pragma runtime_checks("", off)
+// a local challenge client-side server through which the lobby server will determine if the user is locally available through port 9901
+// this should never respond with "YESIMLOCAL" via an online conn! only via VPN and LAN it should give such a response!
+void LocalChallengeServer()
+{
+	bLocalChallengeActive = true;
+	//printf("Starting challenge!\n");
+
+	double TIME_Frequency = 0.0;
+	double startTime = 0.0;
+	constexpr DWORD ChallengeTimeOut = 10000; // 10sec timeout
+	try
+	{
+		UDPSocket Socket;
+		uint32_t query = 0;
+		uint32_t response = 0;
+
+		Socket.Bind(9901);
+		Socket.SetTimeout(ChallengeTimeOut);
+
+		while (bLocalChallengeActive)
+		{
+			sockaddr_in add = Socket.RecvFrom((char*)&query, sizeof(uint32_t));
+			add.sin_family = AF_INET; // force it due to Win11 forcing IPv6...
+
+			if (query == 0x6A093EC9) // strhash of "LOCAL?" 6A093EC9
+			{
+				response = 0x8DB682D1; // strhash of "YESIMLOCAL" 8DB682D1
+				Socket.SendTo(add, (char*)&response, sizeof(uint32_t));
+				bLocalChallengeActive = false;
+			}
+			else
+				bLocalChallengeActive = false;
+		}
+	}
+	catch (std::system_error& e)
+	{
+		bLocalChallengeActive = false;
+		//std::cout << e.what() << '\n';
+	}
+}
+
+bool QueryExternalIP() 
+{
+	bCurrentlyCatchingIP = true;
+	HINTERNET net = InternetOpenA("IP retriever",
+		INTERNET_OPEN_TYPE_PRECONFIG,
+		NULL,
+		NULL,
+		0);
+
+	if (!net)
+	{
+		strcpy(external_addr_str, "Failed to open an internet connection.");
+		bCurrentlyCatchingIP = false;
+		return false;
+	}
+
+	HINTERNET conn = InternetOpenUrlA(net,
+		ipcheckdomain,
+		NULL,
+		0,
+		INTERNET_FLAG_RELOAD,
+		0);
+
+	if (!conn)
+	{
+		sprintf(external_addr_str, "Failed to open: %s\n", ipcheckdomain);
+		bCurrentlyCatchingIP = false;
+		return false;
+	}
+
+	char buffer[4096];
+	DWORD read;
+	BOOL status;
+
+	status = InternetReadFile(conn, buffer, sizeof(buffer) / sizeof(buffer[0]), &read);
+	if (!status)
+	{
+		sprintf(external_addr_str, "Failed to read: %s\n", ipcheckdomain);
+		bCurrentlyCatchingIP = false;
+		return false;
+	}
+	InternetCloseHandle(net);
+
+	uint8_t p1;
+	uint8_t p2;
+	uint8_t p3;
+	uint8_t p4;
+
+	sscanf(buffer, "%hhu.%hhu.%hhu.%hhu", &p1, &p2, &p3, &p4);
+	sprintf(external_addr_str, "%hhu.%hhu.%hhu.%hhu", p1, p2, p3, p4);
+
+	external_addr = p1 << 24 | p2 << 16 | p3 << 8 | p4;
+
+
+	bCurrentlyCatchingIP = false;
+	return true;
+}
+
+bool __stdcall InitConnHook(char* host, uint16_t port, uint32_t timeout)
+{
+	uintptr_t that;
+	_asm mov that, ecx
+
+	if (!bLocalChallengeActive && bOnlineGame)
+		std::thread(LocalChallengeServer).detach();
+
+	return reinterpret_cast<bool(__thiscall*)(uintptr_t, char*, uint16_t, uint32_t)>(0x00792BF0)(that, host, port, timeout);
+}
 
 void InitiateConnection()
 {
@@ -57,25 +177,46 @@ void InitiateConnection()
 	return reinterpret_cast<void(__thiscall*)(uintptr_t, const char*, int32_t, uint32_t, bool)>(0x005257F0)(FEngInstance, "UI_PC_LAN.fng", 0, 0, false); // QueuePackagePush
 }
 
+static void HelpMarker(const char* desc)
+{
+	ImGui::TextDisabled("(?)");
+	if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort) && ImGui::BeginTooltip())
+	{
+		ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+		ImGui::TextUnformatted(desc);
+		ImGui::PopTextWrapPos();
+		ImGui::EndTooltip();
+	}
+}
+
 void DrawIPInputWindow()
 {
 	if (ImGui::BeginPopupModal(POPUP_MODAL_HEADER, nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove))
 	{
 		if (!bIPWindowOnceFlag)
 		{
+			if (!external_addr && !bCurrentlyCatchingIP)
+				std::thread(QueryExternalIP).detach();
 			bUseUPnP = true;
+			bOnlineGame = true;
 			ImGui::SetKeyboardFocusHere();
-			bShownIPWindowOnce = true;
 			bIPWindowOnceFlag = true;
 		}
 
+		ImGui::Text("WAN IP: %s", external_addr_str);
+		ImGui::SameLine(); HelpMarker("Your external IP address.\nNOTE: If this fails, do not panic. This is only critical for users local to the lobby server who want to play with online players!");
 		ImGui::InputText("IP Address", (char*)0x008F42EC, 32);
 		ImGui::InputInt("Port", (int*)0x008F430C);
 		*(int*)0x008F430C &= 0xFFFF;
 
 		ImGui::Checkbox("Use UPnP for host port", &bUseUPnP);
+		ImGui::SameLine(); HelpMarker("Opens a port mapping via UPnP on UDP port 3658.");
+		ImGui::Checkbox("Online", &bOnlineGame);
+		ImGui::SameLine(); HelpMarker("Sends the server your external IP address.\nEnable this if you want to play online (even if the lobby server is in your LAN).");
+		//ImGui::Checkbox("Local lobby", &bLocalConn);
+		//ImGui::SameLine(); HelpMarker("Enable this if the lobby server is on the same network as the game and you want to play online.\nThis will dispatch the server to report its external IP to online players in the lobby instead of your local one.");
 
-		if (ImGui::Button("Connect (Return)", ImVec2(ImGui::GetContentRegionAvail().x / 2.0f, 0)) || (GetAsyncKeyState(VK_RETURN) >> 15))
+		if (ImGui::Button("Connect (Return)", ImVec2(ImGui::GetContentRegionAvail().x / 2.0f, 0)) || (GetAsyncKeyState(VK_RETURN) >> 15) && WndProcWalker::IsWindowActive())
 		{
 			ImGuiIO& io = ImGui::GetIO(); (void)io;
 			io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange;
@@ -85,12 +226,14 @@ void DrawIPInputWindow()
 			InitiateConnection();
 		}
 		ImGui::SameLine();
-		if (ImGui::Button("Cancel (ESC)", ImVec2(ImGui::GetContentRegionAvail().x, 0)) || (GetAsyncKeyState(VK_ESCAPE) >> 15)) // slightly buggy, can cause the menu to show in main menu if you're fast enough
+		if (ImGui::Button("Cancel (ESC)", ImVec2(ImGui::GetContentRegionAvail().x, 0)) || (GetAsyncKeyState(VK_ESCAPE) >> 15) && WndProcWalker::IsWindowActive()) // slightly buggy, can cause the menu to show in main menu if you're fast enough
 		{
 			ImGuiIO& io = ImGui::GetIO(); (void)io;
 			io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange;
 			bShowIPInputWindow = false;
 			bIPWindowOnceFlag = false;
+			bUseUPnP = false;
+			bOnlineGame = false;
 		}
 
 		ImGui::EndPopup();
@@ -330,19 +473,12 @@ uintptr_t SocketPatch1(uint32_t af, uint32_t type, uint32_t protocol)
 	return reinterpret_cast<uintptr_t(*)(uint32_t, uint32_t, uint32_t)>(SocketFunc1)(af, type, 17);
 }
 
-int __stdcall WSAStartupHook(WORD wVersionRequested, LPWSADATA lpWSAData)
+const char* ReturnSKUTag()
 {
-	return WSAStartup(MAKEWORD(2, 2), lpWSAData);
-}
+	if (external_addr)
+		return external_addr_str;
 
-uintptr_t BindFunc = 0x0084AFB0;
-int bindhook(SOCKET s, const sockaddr* name, int namelen)
-{
-	sockaddr_in* add = (sockaddr_in*)name;
-	add->sin_family = AF_INET;
-	add->sin_addr.s_addr = htonl(INADDR_ANY);
-
-	return reinterpret_cast<int(*)(SOCKET, const sockaddr*, int)>(SocketFunc1)(s, name, namelen);
+	return "SKU_LANIP";
 }
 
 #pragma runtime_checks("", restore)
@@ -376,8 +512,15 @@ int Init()
 	// TODO - remove / workaround this if the online mode ever gets revived
 	injector::WriteMemory<uint32_t>(0x008C7ADC, 0, true);
 
-	// patch the UDP bind to use any addr -- needed for internet connections
-	BindFunc = (uintptr_t)injector::MakeCALL(0x00850F8C, bindhook).get_raw<void>();
+	// skip UDP bind of client socket
+	injector::MakeJMP(0x00851661, 0x8516CA);
+
+	injector::MakeCALL(0x00794A52, ReturnSKUTag);
+
+	injector::MakeCALL(0x00553AB5, InitConnHook);
+	injector::MakeCALL(0x00553AFA, InitConnHook);
+
+	strcpy(external_addr_str, "Querying IP...");
 
 	return 0;
 }
